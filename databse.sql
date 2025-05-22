d38bf7e53da6a89e0681ad1e178eb340b404834e
cmitsakopoulos <mitsacopoulos@gmail.com>
2025-05-22 16:30:40 +0300

    major fixes

--- databse.sql
+++ databse.sql
@@ -1,18 +1,19 @@
 BEGIN;
-DROP TABLE IF EXISTS cdr3_primary;
-DROP TABLE IF EXISTS cdr3_central;
-DROP TABLE IF EXISTS antigen_central;
-DROP TABLE IF EXISTS antigen_primary;
 DROP TABLE IF EXISTS antigen_antibody_binding;
+DROP TABLE IF EXISTS antigen_primary;
+DROP TABLE IF EXISTS cdr3_primary;
+DROP TABLE IF EXISTS antigen_central;
+DROP TABLE IF EXISTS cdr3_central;
 
 CREATE TABLE antigen_central (
-    antigen_id          INT      PRIMARY KEY, 
+    antigen_id          INT, 
     name                TEXT,
     species             TEXT,
     method              TEXT,
-    taxonomy_id         INT,
+    taxonomy_id         TEXT,
     resolution_angstrom DOUBLE PRECISION,
-    last_update         TIMESTAMPTZ
+    last_update         TIMESTAMPTZ,
+    CONSTRAINT pk_antigen_id_main PRIMARY KEY (antigen_id)
 );
 
 CREATE TABLE antigen_primary (
@@ -24,19 +25,20 @@ CREATE TABLE antigen_primary (
     net_charge_inflammation DOUBLE PRECISION,
     CONSTRAINT fk_antigen_primary_to_central
       FOREIGN KEY (antigen_id)
-      REFERENCES antigen_central(antigen_id)
+      REFERENCES antigen_central(antigen_id) ON DELETE CASCADE
 );
 
 CREATE INDEX index_antigen_with_cdr3
   ON antigen_primary (antigen_id); --Indexing should help with lookup speed especially when automating look up commands for training an AI model....but might need to remove if overhead is too much; if too much ram usage/space usage
 
 CREATE TABLE cdr3_central (
-    cdr3_id             INT    PRIMARY KEY,
+    cdr3_id             INT,
     method              TEXT,
     resolution_angstrom DOUBLE PRECISION,
     species             TEXT,
-    taxonomy_id         INT,
-    last_update         TIMESTAMPTZ);
+    taxonomy_id         TEXT,
+    last_update         TIMESTAMPTZ,
+    CONSTRAINT pk_cdr3_id_main PRIMARY KEY (cdr3_id));
 
 CREATE TABLE cdr3_primary (
     cdr3_id     INT PRIMARY KEY,
@@ -47,7 +49,7 @@ CREATE TABLE cdr3_primary (
     net_charge_inflamed DOUBLE PRECISION,
     CONSTRAINT fk_cdr3_primary_to_central
       FOREIGN KEY (cdr3_id)
-      REFERENCES cdr3_central(cdr3_id)
+      REFERENCES cdr3_central(cdr3_id) ON DELETE CASCADE
 );
 
 CREATE TABLE antigen_antibody_binding (
--- staging.sql
+++ staging.sql
@@ -4,7 +4,7 @@ CREATE TABLE staging_antigen (
   database_origin               TEXT,
   antigen_organism_name         TEXT,
   antigen_host_organism         TEXT,
-  antigen_taxonomy_id           INT,
+  antigen_taxonomy_id           TEXT,
   resolution                    DOUBLE PRECISION,
   method                        TEXT,
   last_update                   TIMESTAMPTZ,
@@ -13,11 +13,57 @@ CREATE TABLE staging_antigen (
   antigen_pI                    DOUBLE PRECISION,
   antigen_net_charge_inflamed   DOUBLE PRECISION,
   antigen_net_charge_normal     DOUBLE PRECISION,
-  antigen_is_incomplete         SMALLINT,
+  antigen_is_incomplete         FLOAT,
   antigen_computed_id           INT
 );
+
+-- COMMIT THE CSV INTO SQL
+\copy staging_antigen
+  FROM '/Users/chrismitsacopoulos/Desktop/Pasteur_Internship/Computation_Deposit/antigen_20250522_125947.csv'
+  WITH (
+    FORMAT csv,
+    HEADER,
+    NULL ''            
+  );
 
 --Because of how I set up the int as a boolean flag within the pipeline, we will have to stick with this method
 ALTER TABLE staging_antigen
   ALTER COLUMN antigen_is_incomplete TYPE BOOLEAN
   USING (antigen_is_incomplete = 1);
+
+CREATE TABLE staging_cdr (
+  h3_chain                      TEXT,
+  database_origin               TEXT,
+  heavy_taxonomy_id             TEXT,
+  heavy_host_organism_name      TEXT,
+  resolution                    DOUBLE PRECISION,
+  method                        TEXT,
+  last_update                   TIMESTAMPTZ,
+  l3_chain                      TEXT,
+  pdb_id                        TEXT,      
+  h3_gravy                      DOUBLE PRECISION,
+  h3_pI                         DOUBLE PRECISION,
+  h3_net_charge_inflamed        DOUBLE PRECISION,
+  h3_net_charge_normal          DOUBLE PRECISION,
+  l3_gravy                      DOUBLE PRECISION,
+  l3_pI                         DOUBLE PRECISION,
+  l3_net_charge_inflamed        DOUBLE PRECISION,
+  l3_net_charge_normal          DOUBLE PRECISION,
+  h3_is_incomplete              FLOAT, 
+  h3_N_gylcosylation_sites      FLOAT,
+  h3_O_gylcosylation_sites      FLOAT,
+  l3_is_incomplete              FLOAT,
+  l3_N_gylcosylation_sites      FLOAT,
+  l3_O_gylcosylation_sites      FLOAT,
+  cdr_computed_id               INT
+);
+
+\copy staging_cdr
+  FROM '/Users/chrismitsacopoulos/Desktop/Pasteur_Internship/Computation_Deposit/cdr_20250522_125947.csv'
+  WITH (
+    FORMAT csv,
+    HEADER,
+    NULL ''            
+  );
+
+ALTER TABLE staging_cdr ALTER COLUMN cdr_is_incomplete TYPE BOOLEAN USING (cdr_is_incomplete = 1);
